f0cea9a760138e75a672aeeb43e724393f840e9d
Malte Reißig <malte@deepamehta.org>
2013-09-16 23:06:09 +0200

    1.0: Initial commit for 4.1.1-SNAPSHOT

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# DeepaMehta User Profiles Plugin
+
+This plugin introduces a model to realize user profiles with a DeepaMehta "User Account". It adds a "Display Name", many "Contact" entries and one "Info" (HTML) to each DeepaMehta "User Account". If an "E-Mail"-Adress is not already part of a "User Account" type definition (mode), this plugin will do so in Migration1.
+
+# Plugins History
+
+1.0.0, Sep 16, 2013
+
+- Plugin Setup, Introducing Migrations
+- Compatible with 
+
+Author: Malte Reißig
+
---
+++ src/main/java/org/deepamehta/plugins/identity/migrations/Migration1.java
@@ -0,0 +1,39 @@
+package org.deepamehta.plugins.identity.migrations;
+
+import de.deepamehta.core.AssociationDefinition;
+import de.deepamehta.core.TopicType;
+import de.deepamehta.core.model.AssociationDefinitionModel;
+import de.deepamehta.core.service.Migration;
+import java.util.Collection;
+import java.util.logging.Logger;
+
+public class Migration1 extends Migration {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    private String USER_ACCOUNT_TYPE_URI = "dm4.accesscontrol.user_account";
+    private static String MAILBOX_TYPE_URI = "dm4.contacts.email_address";
+
+    @Override
+    public void run() {
+
+        TopicType account = dms.getTopicType(USER_ACCOUNT_TYPE_URI, null);
+
+		// 3) If not already done, enrich the "User Account"-Type about a "Person"-Type
+        Collection<AssociationDefinition> childTypes = account.getAssocDefs();
+        boolean hasPersonAsChild = false;
+        for (AssociationDefinition child : childTypes) {
+            if (child.getChildTypeUri().equals(MAILBOX_TYPE_URI)) hasPersonAsChild = true;
+        }
+        if (!hasPersonAsChild) {
+            logger.info("Identity Migration1 => Enriching \"User Account\"-Type about \"Mailbox\"-Type");
+            account.addAssocDef(new AssociationDefinitionModel("dm4.core.aggregation_def", USER_ACCOUNT_TYPE_URI,
+                MAILBOX_TYPE_URI, "dm4.core.one", "dm4.core.one"));
+        } else {
+            logger.info("Identity Migration1 => NOT Enriching \"User Account\"-Type about \"Mailbox\"-Type - "
+                    + "Already done!");
+        }
+
+    }
+
+}
